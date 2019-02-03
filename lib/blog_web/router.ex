3ec82f5a7a53f095dfd29ab5267c35cb53fdb3b6
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2019-02-03 15:05:34 +0900

    GraphQLのスキーマ作成

---
+++ lib/blog_web/router.ex
@@ -0,0 +1,33 @@
+defmodule BlogWeb.Router do
+  use BlogWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", BlogWeb do
+    pipe_through :browser
+
+    get "/", PageController, :index
+    resources "/posts", PostController
+  end
+
+  scope "/api" do
+    pipe_through :api
+
+    forward "/graphiql", Absinthe.Plug.GraphiQL,
+      schema: BlogWeb.Schema
+
+    forward "/", Absinthe.Plug,
+      schema: BlogWeb.Schema
+
+  end
+end
